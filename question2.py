b3ca6de404cb8d29a1821b5dd2b200268a3fe1e6
Will <wyliao76@gmail.com>
2018-10-18 11:15:09 +0800

    Question2 done

---
+++ question2.py
@@ -0,0 +1,17 @@
+
+
+def anonymous(x):
+    return x**2 + 1
+
+
+def integrate(fun, start, end):
+    step = 0.1
+    intercept = start
+    area = 0
+    while intercept < end:
+        intercept += step
+        area = anonymous(intercept)
+        return area
+
+
+print(integrate(anonymous, 0, 10))
